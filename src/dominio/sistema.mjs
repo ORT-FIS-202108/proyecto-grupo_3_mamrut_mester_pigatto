88b6cac5a87247e5c576622c22e26e20c87ff9fa
ivanamester <ivanamester@hotmail.com>
2021-11-17 19:07:08 -0300

    arreglar formatos eslint

--- src/dominio/sistema.mjs
+++ src/dominio/sistema.mjs
@@ -1,48 +1,53 @@
 export class Sistema {
-    constructor() {
-      this.gastos = []
-      this.ingresos = []
-      this.recordatorios = []
-      this.mediosDePago = []
-      this.conceptos = []
-    }
-  
-    agregarGasto(gasto) {
-      this.gastos.push(gasto);
-    }
-  
-    agregarIngreso(ingreso) {
-      this.ingresos.push(ingreso);
-    }
-  
-    agregarRecordatorio(recordatorio) {
-      this.recordatorios.push(recordatorio);
-    }
-  
-    agregarMedioDePago(medioDePago) {
-      this.mediosDePago.push(medioDePago);
-    }
-  
-    agregarConcepto(concepto) {
-      this.conceptos.push(concepto);
-    }
-  
-    ordenarGastosPorFecha() { //Ordena de mas recientes a mas antiguos
-      this.gastos.sort(function (g1, g2) { return g2.fecha - g1.fecha });
-    }
-
-    ordenarIngresosPorFecha() { //Ordena de mas recientes a mas antiguos
-      this.ingresos.sort(function (g1, g2) { return g2.fecha - g1.fecha });
-    }
-
-    ordenarRecordatoriosPorFecha() { //Ordena de mas recientes a mas antiguos
-      this.recordatorios.sort(function (g1, g2) { return g2.fecha - g1.fecha });
-    }
-
-    borrarElemento(lista, pos){
-      lista.splice(pos,1);
-    }
-  
-    //Hacer funciones para ordenar ingresos y recordatorios cuando veamos que funciona la de gasto
-  
+  constructor() {
+    this.gastos = [];
+    this.ingresos = [];
+    this.recordatorios = [];
+    this.mediosDePago = [];
+    this.conceptos = [];
   }
+
+  agregarGasto(gasto) {
+    this.gastos.push(gasto);
+  }
+
+  agregarIngreso(ingreso) {
+    this.ingresos.push(ingreso);
+  }
+
+  agregarRecordatorio(recordatorio) {
+    this.recordatorios.push(recordatorio);
+  }
+
+  agregarMedioDePago(medioDePago) {
+    this.mediosDePago.push(medioDePago);
+  }
+
+  agregarConcepto(concepto) {
+    this.conceptos.push(concepto);
+  }
+
+  ordenarGastosPorFecha() { // Ordena de mas recientes a mas antiguos
+    this.gastos.sort(function(g1, g2) {
+      return g2.fecha - g1.fecha;
+    });
+  }
+
+  ordenarIngresosPorFecha() { // Ordena de mas recientes a mas antiguos
+    this.ingresos.sort(function(g1, g2) {
+      return g2.fecha - g1.fecha;
+    });
+  }
+
+  ordenarRecordatoriosPorFecha() { // Ordena de mas recientes a mas antiguos
+    this.recordatorios.sort(function(g1, g2) {
+      return g2.fecha - g1.fecha;
+    });
+  }
+
+  borrarElemento(lista, pos) {
+    lista.splice(pos, 1);
+  }
+
+  // Hacer funciones para ordenar ingresos y recordatorios cuando veamos que funciona la de gasto
+}
--- src/dominio/sistema.test.js
+++ src/dominio/sistema.test.js
@@ -2,10 +2,10 @@ import Sistema from './sistema.mjs';
 
 
 test('creación de un sistema', () => {
-    let unSistema = new Sistema();
-    expect(unSistema.gastos).toBe([]);
-    expect(unSistema.ingresos).toBe([]);
-    expect(unSistema.recordatorios).toBe([]);
-    expect(unSistema.mediosDePago).toBe([]);
-    expect(unSistema.conceptos).toBe([]);
-  });
+  const unSistema = new Sistema();
+  expect(unSistema.gastos).toBe([]);
+  expect(unSistema.ingresos).toBe([]);
+  expect(unSistema.recordatorios).toBe([]);
+  expect(unSistema.mediosDePago).toBe([]);
+  expect(unSistema.conceptos).toBe([]);
+});
